15d64fbc065d0f323ce9049020cd9fdc7383634a
jennilaluyan <laluyanjennifer11@gmail.com>
2025-06-16 12:59:39 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="font-bold text-5xl underline">This is remembrance day invitation web for Elko and Joppy Langi</h1>
+    </>
+  )
+}
+
+export default App;
